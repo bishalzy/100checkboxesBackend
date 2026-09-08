63b8f63af54645e3bd6189b2767cdef01b021943
Bishal <bishaladhikari872@gmail.com>
2026-09-08 18:01:28 +0545

    Change server port from 3000 to 3001

--- src/index.js
+++ src/index.js
@@ -58,7 +58,7 @@ wss.on('connection', (socket) => {
 });
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 server.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
